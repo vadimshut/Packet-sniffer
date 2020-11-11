79440f0fb21934841eb765dcf8deb25d47445be0
Vadim <vadimshut1992@gmail.com>
2020-11-11 03:40:11 -0500

    Add check for correct input of network interface

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -5,9 +5,9 @@ from scapy.layers import http
 from colorama import Fore, init
 from datetime import datetime
 import subprocess
+import argparse
 
 init(autoreset=True)
-iface = "eth0"
 
 
 def get_list_interfaces():
@@ -16,13 +16,27 @@ def get_list_interfaces():
     list_ifconfig = output.decode().split("\n")
     for i in list_ifconfig:
         if len(i.split(": flags=")) > 1:
-            interface = i.split(": flags=")[0].strip()
-            list_interfaces.append(interface)
+            item_interface = i.split(": flags=")[0].strip()
+            list_interfaces.append(item_interface)
     return list_interfaces
 
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=packet_analysis)
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Enter the interface name, for example: eth0")
+    options = parser.parse_args()
+    list_interfaces = get_list_interfaces()
+
+    if not options.interface:
+        parser.error(Fore.RED + "[+]" + Fore.GREEN + " Please specify network interface, use --help for more info")
+    if options.interface not in list_interfaces:
+        parser.error(Fore.RED + "[+]" + Fore.GREEN + f" Please specify correct network interface, from the following: "
+                                                     f"{', '.join(list_interfaces)}")
+    return options.interface
+
+
+def sniff(name_interface):
+    scapy.sniff(iface=name_interface, store=False, prn=packet_analysis)
 
 
 def geturl(packet):
@@ -47,11 +61,13 @@ def packet_analysis(packet):
     if packet.haslayer(http.HTTPRequest):
         url = geturl(packet)
         get_time_now()
-        print(f"[+] HTTP Request >> {url}")
+        print(f"[+] HTTP Request >> {url.decode()}")
         login_info = get_login_info(packet)
         if login_info:
             get_time_now()
             print(Fore.YELLOW + f"[+] Possible username/password {login_info}")
 
 
-sniff(iface)
+if __name__ == "__main__":
+    interface = get_arguments()
+    sniff(interface)
